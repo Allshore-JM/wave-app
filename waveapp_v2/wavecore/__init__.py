7017f741380c9f42e07fbc1250d85cc06485c98d
Allshore-JM <joshmurphree@gmail.com>
2025-08-24 06:09:51 -0700

    Fix extra closing bracket in __all__ list

--- waveapp_v2/wavecore/__init__.py
+++ waveapp_v2/wavecore/__init__.py
@@ -3,4 +3,4 @@ wavecore package for waveapp_v2
 """
 from .timeindex import hourly_series_from_day_hour, to_local
 
-__all__ = ["hourly_series_from_day_hour", "to_local"]]
+__all__ = ["hourly_series_from_day_hour", "to_local"]
